739ca38a48cd169ce412d84f488dd96aa965d63f
taehoshino <wildsheepchase0211@gmail.com>
2020-09-23 11:13:32 +1000

    more changes

--- public/js/frontendapp.js
+++ public/js/frontendapp.js
@@ -5,7 +5,7 @@ console.log(client_id);
 for (var i=0; i<url_list.length; i++) {
     console.log(i);
     if (url_list[i].includes("https://")) {
-        send_url = url_list[i]; 
+        var send_url = url_list[i]; 
         break; 
     }
 }
@@ -13,25 +13,26 @@ send_url = send_url.replace(/&amp;/g, "&")
 
 $(document).ready(function(){
     
-    $('.submenu h3').on('click', function(){
-        $(this).next().toggleClass('hidden');
-    });
+    // $('.submenu h3').on('click', function(){
+    //     $(this).next().toggleClass('hidden');
+    // });
 
-    var li = document.createElement('li');
+    // var li = document.createElement('li');
+    var headerElem = document.createElement('h5')
     var btn = $('<input/>').attr({type: 'button', id: 'btnRender'});
 
     if (i===0){
-        li.textContent = "Signature request - Sign URL: " + send_url; 
+        headerElem.textContent = "You have received signature request!";
         btn.attr({value: "Sign Now!"});
     } else if (i===1) {
-        li.textContent = "Create template - Edit URL: " + send_url; 
+        headerElem.textContent = "You can create template!";
         btn.attr({value: "Create Template Now!"});
     } else {
-        li.textContent = "Unclaimed draft - Claim URL: " + send_url; 
+        headerElem.textContent = "You can send signature request!";
         btn.attr({value: "Send Request Now!"});
     }
-    document.getElementById('list').appendChild(li);
-    $('#list').append(btn);
+    $('.main-content').append(headerElem);
+    $('.main-content').append(btn);
 
     $('#btnRender').click(function(){
         const client = new HelloSign({
@@ -43,7 +44,6 @@ $(document).ready(function(){
         testMode: true,
         skipDomainVerification: true, 
         debug: true
-        // container: document.getElementById("signatureContainer")
         });
 
     })
--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const path = require("path")
 const chalk = require("chalk")
 const {send_signature_request, send_signature_request_with_template, create_template_draft, create_unclaimed_draft, create_unclaimed_draft_with_template} = require("./request")
 const {client_id, signer_email, requester_email, template_id} = require("./config")
+const { title } = require("process")
+const { sign } = require("crypto")
 
 // assign variables to user input
 const command = process.argv[2]
@@ -31,20 +33,29 @@ var sign_url = undefined
 var edit_url = undefined
 var claim_url = undefined
 
+const funcRender = (sign_url, edit_url, claim_url) => {
+    app.get("/", (req, res)=>{
+        return res.render("index", {
+            client_id, 
+            sign_url, 
+            edit_url, 
+            claim_url,
+            title: "API Request Options", 
+            message: "This is Hellosign Embedded API testing tool!"
+        })
+    })
+}
+
 // call functions based on user input
 if (command === "send") {
     if (opt === "template") {
         send_signature_request_with_template(template_id, signer_email, (sign_url = undefined)=>{
-            app.get("/", (req, res)=>{
-                return res.render("index", {client_id, sign_url, edit_url, claim_url})
-            })
+            funcRender(sign_url, edit_url, claim_url)
         })
 
     } else {
         send_signature_request(signer_email, (sign_url = undefined)=>{
-            app.get("/", (req, res)=>{
-                return res.render("index", {client_id, sign_url, edit_url, claim_url})
-            })        
+            funcRender(sign_url, edit_url, claim_url)
         })
         
     }
@@ -52,35 +63,34 @@ if (command === "send") {
 } else if (command === "unclaimed") {
     if (opt === "template") {
         create_unclaimed_draft_with_template(template_id, requester_email, (claim_url = undefined)=>{
-            app.get("/", (req, res)=>{
-                return res.render("index", {client_id, sign_url, edit_url, claim_url})
-        })
+            funcRender(sign_url, edit_url, claim_url)
         })
 
     } else {
         create_unclaimed_draft(requester_email, (claim_url = undefined)=>{
-            app.get("/", (req, res) => {
-                return res.render("index", {client_id, sign_url, edit_url, claim_url})
-            })
+            funcRender(sign_url, edit_url, claim_url)
         })
         
     }
 
 } else if (command === "template") {
     create_template_draft((template_id = undefined, edit_url = undefined)=>{
-        app.get("/", (req, res)=>{
-            return res.render("index", {client_id, sign_url, edit_url, claim_url})
-        })
+        funcRender(sign_url, edit_url, claim_url)
     })
     
 } else {
     console.log(chalk.red.inverse("Invalid command value!"))
-    app.get("/", (req, res)=>{
-        return res.render("index", {client_id, sign_url, edit_url, claim_url})
-    })
+    funcRender(sign_url, edit_url, claim_url)
 }
 
+// Resouces page
+app.get("/resources", (req, res)=>{
+    res.render("resources", {
+        title: "Learning Resources",
+        message: "Here are some useful links." 
+    })
 
+})
 
 // instantiate local server at port 5000
 const server = http.createServer(app)
